859ba9d41877cfedab5fa9959e9fdded97650a8a
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2022-04-17 17:47:18 +0200

    Remove the usage of loca.properties

--- iptv_network/src/main/java/de/itshasan/iptv_repository/network/IptvRepository.kt
+++ iptv_network/src/main/java/de/itshasan/iptv_repository/network/IptvRepository.kt
@@ -29,9 +29,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 object IptvRepository : IptvRepositoryContract {
 
     private val TAG = IptvRepository::class.java.simpleName
-//    private const val baseUrl: String = BuildConfig.IPTV_SERVER
-//    private const val username: String = BuildConfig.IPTV_USERNAME
-//    private const val password: String = BuildConfig.IPTV_PASSWORD
 
     private var gson = GsonBuilder()
         .setLenient()
